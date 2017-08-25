4d2ac3a0a33a61248243810ff134e835ceb56b47
ScoTech <nich.scoville@gmail.com>
2017-08-25 14:20:34 -0400

    v 1.03

--- TreeArm/main.c
+++ TreeArm/main.c
@@ -36,26 +36,25 @@
 #include "driverlib/timer.h"
 #include "driverlib/interrupt.h"
 
-
 //*****************************************************************************************
 //  Definitions
 //*****************************************************************************************
-#define TIMER0LOAD      0x000A2C2A    //Load Value for Timer0,     ~60Hz at 40MHz Sys Clock
-#define LED_SIZE        100
-#define UART_BUF_SIZE   96
-#define FPS             60
+#define TIMER0LOAD          0x000A2C2A    //Load Value for Timer0,     ~60Hz at 40MHz Sys Clock
+#define LED_SIZE            100
+#define UART_BUF_SIZE       96
+#define MAX_FRAMES          25
+#define FPS                 4
 
 //*****************************************************************************************
 //  Global Variables
 //*****************************************************************************************
 bool Service_Timer0 = false;   //Flag for Timer 0 Interrupt Service
-static uint8_t  LEDBufA[LED_SIZE][3];
-static uint8_t  LEDBufB[LED_SIZE][3];
-static uint8_t  TxBufA[UART_BUF_SIZE];
-static uint8_t  TxBufB[UART_BUF_SIZE];
+static uint8_t LEDBufA[LED_SIZE][3];
+static uint8_t LEDBufB[LED_SIZE][3];
+static uint8_t TxBufA[UART_BUF_SIZE];
+static uint8_t TxBufB[UART_BUF_SIZE];
 static uint32_t uDMAErrCount = 0;
-static uint32_t TXBufACount = 0;
-static uint32_t RXBufBCount = 0;
+static uint32_t num_frames = 0;
 static int count = 0;
 static char str[32];               //String to format results
 static tContext sContext;
@@ -85,14 +84,15 @@ uint8_t ui8ControlTable[1024] __attribute__ ((aligned(1024)));
 void Timer0IntHandler(void)
 {
     TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT); //Clear Timer0 Interrupt Flag
-    IntMasterDisable();                                 //Disable Interrupts
-    Service_Timer0 = true;
-    IntMasterEnable();                                  //Enable Interrupts
+    //Do Some Work!!!                             //Enable Interrupts
 }
-
 
 void SysTickHandler(void)
 {
+    IntMasterDisable();                                 //Disable Interrupts
+    Service_Timer0 = true;
+    count += 1;
+    IntMasterEnable();
 }
 
 void uDMAErrorHandler(void)
@@ -108,7 +108,7 @@ void uDMAErrorHandler(void)
     // If there is a uDMA error, then clear the error and increment
     // the error counter.
     //
-    if(ui32Status)
+    if (ui32Status)
     {
         uDMAErrorStatusClear();
         uDMAErrCount++;
@@ -121,43 +121,70 @@ void UART4IntHandler(void)
 
     UARTStatus = UARTIntStatus(UART4_BASE, 1);
     UARTIntClear(UART4_BASE, UARTStatus);
-    uDMAModeA = uDMAChannelModeGet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT);
-    uDMAModeB = uDMAChannelModeGet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT);
-    if(uDMAModeA == UDMA_MODE_STOP)
-    {
-        uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT,
-                                       UDMA_MODE_PINGPONG,TxBufA,
-                                       (void *)(UART4_BASE), UART_BUF_SIZE);
-    }
-    else if(uDMAModeB == UDMA_MODE_STOP)
-    {
-        uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_ALT_SELECT,
-                                       UDMA_MODE_PINGPONG,TxBufB,
-                                       (void *)(UART4_BASE), UART_BUF_SIZE);
-    }
-    else
-    {
-        uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT,
-                                       UDMA_MODE_PINGPONG,TxBufA,
-                                       (void *)(UART4_BASE), UART_BUF_SIZE);
-        uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_ALT_SELECT,
-                                       UDMA_MODE_PINGPONG,TxBufB,
-                                       (void *)(UART4_BASE), UART_BUF_SIZE);
 
-        uDMAChannelEnable(UDMA_CHANNEL_TMR0B);
-    }
     IntMasterDisable();
-    count+=1;
+    //if (num_frames && (num_frames < MAX_FRAMES))
+    //if(1)
+   //{
+        uDMAModeA = uDMAChannelModeGet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT);
+        uDMAModeB = uDMAChannelModeGet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT);
+
+        if (uDMAModeA == UDMA_MODE_STOP)
+        {
+
+            uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT,
+            UDMA_MODE_PINGPONG,
+                                   TxBufA, (void *) (UART4_BASE),
+                                   UART_BUF_SIZE);
+            num_frames++;
+        }
+        else if (uDMAModeB == UDMA_MODE_STOP)
+        {
+            uDMAChannelTransferSet(
+            UDMA_CHANNEL_TMR0B | UDMA_ALT_SELECT,
+                                   UDMA_MODE_PINGPONG, TxBufB,
+                                   (void *) (UART4_BASE), UART_BUF_SIZE);
+            num_frames++;
+        }
+
+        else
+        {
+/*            uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT,
+            UDMA_MODE_PINGPONG,
+                                   TxBufA, (void *) (UART4_BASE), UART_BUF_SIZE);
+            uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_ALT_SELECT,
+            UDMA_MODE_PINGPONG,
+                                   TxBufB, (void *) (UART4_BASE), UART_BUF_SIZE);
+
+            uDMAChannelEnable(UDMA_CHANNEL_TMR0B);
+*/        }
+    //}
+    //count+=1;
+        uDMAChannelEnable(UDMA_CHANNEL_TMR0B);
     IntMasterEnable();
 }
 
+void send_frame(void)
+{
+    uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT,
+    UDMA_MODE_PINGPONG,
+                           TxBufA, (void *) (UART4_BASE), UART_BUF_SIZE);
+    uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_ALT_SELECT,
+    UDMA_MODE_PINGPONG,
+                           TxBufB, (void *) (UART4_BASE), UART_BUF_SIZE);
+
+    uDMAChannelEnable(UDMA_CHANNEL_TMR0B);
+    num_frames = 2;
+}
 
 void init_clock(void)
 {
     //
     //Setup System Click and System Tick to 40Mhz and 60Hz
     //
-    SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+    SysCtlClockSet(
+            SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN
+                    | SYSCTL_XTAL_16MHZ);
     SysCtlPeripheralClockGating(true);
     SysTickPeriodSet(SysCtlClockGet() / FPS);
     SysTickIntEnable();
@@ -175,22 +202,24 @@ void init_display(void)
 void init_gpo(void)
 {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ))
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ))
     {
     }
     SysCtlPeripheralSleepEnable(SYSCTL_PERIPH_GPIOJ);
     GPIOPinTypeUART(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-    GPIOPinConfigure((GPIO_PJ0_U4RX|GPIO_PJ1_U4TX));
+    GPIOPinConfigure((GPIO_PJ0_U4RX | GPIO_PJ1_U4TX));
 }
 
 void init_UART(void)
 {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART4);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART4))
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_UART4))
     {
     }
     SysCtlPeripheralSleepEnable(SYSCTL_PERIPH_UART4);
-    UARTConfigSetExpClk(UART4_BASE, SysCtlClockGet(), 4000000, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+    UARTConfigSetExpClk(
+            UART4_BASE, SysCtlClockGet(), 4000000,
+            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
     UARTFIFOLevelSet(UART4_BASE, UART_FIFO_TX4_8, UART_FIFO_RX4_8);
     UARTEnable(UART4_BASE);
     UARTDMAEnable(UART4_BASE, UART_DMA_TX);
@@ -204,7 +233,7 @@ void init_UART(void)
 void init_uDMA(void)
 {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UDMA);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UDMA))
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_UDMA))
     {
     }
     SysCtlPeripheralSleepEnable(SYSCTL_PERIPH_UDMA);
@@ -213,18 +242,17 @@ void init_uDMA(void)
     uDMAControlBaseSet(ui8ControlTable);
     uDMAChannelAssign(UDMA_CH19_UART4TX);
 
-
     uDMAChannelAttributeDisable(UDMA_CHANNEL_TMR0B,
-                                        UDMA_ATTR_ALTSELECT | UDMA_ATTR_USEBURST |
-                                        UDMA_ATTR_HIGH_PRIORITY |
-                                        UDMA_ATTR_REQMASK);
+    UDMA_ATTR_ALTSELECT | UDMA_ATTR_USEBURST |
+    UDMA_ATTR_HIGH_PRIORITY |
+    UDMA_ATTR_REQMASK);
     uDMAChannelAttributeEnable(UDMA_CHANNEL_TMR0B, UDMA_ATTR_USEBURST);
     uDMAChannelControlSet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT,
-                                  UDMA_SIZE_8 | UDMA_SRC_INC_8 | UDMA_DST_INC_NONE |
-                                  UDMA_ARB_4);
+    UDMA_SIZE_8 | UDMA_SRC_INC_8 | UDMA_DST_INC_NONE |
+    UDMA_ARB_4);
     uDMAChannelControlSet(UDMA_CHANNEL_TMR0B | UDMA_ALT_SELECT,
-                                  UDMA_SIZE_8 | UDMA_SRC_INC_8 | UDMA_DST_INC_NONE |
-                                  UDMA_ARB_4);
+    UDMA_SIZE_8 | UDMA_SRC_INC_8 | UDMA_DST_INC_NONE |
+    UDMA_ARB_4);
 
 }
 
@@ -253,10 +281,11 @@ void init_timer(void)
 //*****************************************************************************************
 int main(void)
 {
-    for(count=0; count < UART_BUF_SIZE; count++)
+    for (count = 0; count < UART_BUF_SIZE; count++)
     {
-        TxBufA[count] =   0xFE;
-        TxBufB[count] =   0xF0;
+        TxBufA[count] = 0xFE;
+        TxBufB[count] = 0xF0;
+        //TxBufB[count] =   0xF0;
         //TxBufA[count] =   0xAA;
         //TxBufB[count] =   0x55;
     }
@@ -271,51 +300,41 @@ int main(void)
     init_UART();
     init_timer();
 
-
-    uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT,
-                                   UDMA_MODE_PINGPONG,TxBufA,
-                                   (void *)(UART4_BASE), UART_BUF_SIZE);
-    uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_ALT_SELECT,
-                                   UDMA_MODE_PINGPONG,TxBufB,
-                                   (void *)(UART4_BASE), UART_BUF_SIZE);
-
-    uDMAChannelEnable(UDMA_CHANNEL_TMR0B);
-
-
     sprintf(str, "Clock: %d", SysCtlClockGet());   //format result string
     GrStringDrawCentered(&sContext, str, -1,
                          GrContextDpyWidthGet(&sContext) / 2, 15, 1); //Draw line 1
 
     IntMasterEnable();                                  //Enable Interrupts
-
+    send_frame();
     while (1)
     {
         if (Service_Timer0)
         {
+            IntMasterDisable();                             //Disable Interrupts
             Service_Timer0 = false;     //Reset Timer0 Interrupt Service Flag
+            IntMasterEnable();
 
-
-            sprintf(str, "Seconds: %d", count);   //format result string
+            sprintf(str, "Seconds: %d", num_frames);   //format result string
             GrStringDrawCentered(&sContext, str, -1,
                                  GrContextDpyWidthGet(&sContext) / 2, 25, 1); //Draw line 1
-            //count+=1;
+
+
             /*uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_PRI_SELECT,
-                                           UDMA_MODE_PINGPONG,TxBufA,
-                                           (void *)(UART4_BASE), UART_BUF_SIZE);
-            uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_ALT_SELECT,
-                                           UDMA_MODE_PINGPONG,TxBufB,
-                                           (void *)(UART4_BASE), UART_BUF_SIZE);*/
+             UDMA_MODE_PINGPONG,TxBufA,
+             (void *)(UART4_BASE), UART_BUF_SIZE);
+             uDMAChannelTransferSet(UDMA_CHANNEL_TMR0B | UDMA_ALT_SELECT,
+             UDMA_MODE_PINGPONG,TxBufB,
+             (void *)(UART4_BASE), UART_BUF_SIZE);*/
             //uDMAChannelEnable(UDMA_CHANNEL_TMR0B);
             //uDMAChannelEnable(UDMA_CHANNEL_TMR0B);
             //UARTBreakCtl(UART4_BASE, false);
-           // UARTCharPutNonBlocking (UART4_BASE, 0xFE);
+            // UARTCharPutNonBlocking (UART4_BASE, 0xFE);
             //UARTCharPutNonBlocking (UART4_BASE, 0xEF);
-           // UARTCharPutNonBlocking (UART4_BASE, 0xFE);
+            // UARTCharPutNonBlocking (UART4_BASE, 0xFE);
             //UARTCharPutNonBlocking (UART4_BASE, 0xF0);
-           // UARTCharPutNonBlocking (UART4_BASE, 0xFE);
+            // UARTCharPutNonBlocking (UART4_BASE, 0xFE);
             //UARTCharPutNonBlocking (UART4_BASE, 0xF0);
             //UARTCharPutNonBlocking (UART4_BASE, 0xFE);
-
             //UARTBreakCtl(UART4_BASE, true);
         }
 
